08c8a0f628aacb335ef76de0accc37ef61be3781
ryan-neil <ryanjosephneil@gmail.com>
2022-05-27 11:50:31 -1000

    add: update and delete

--- react/apps/rtk-query-backend/server/controllers/users.controller.ts
+++ react/apps/rtk-query-backend/server/controllers/users.controller.ts
@@ -20,14 +20,84 @@ const getUser = (req: Request, res: Response) => {
   const foundUser = data.users.find(
     (user: IUser) => user.id === parseInt(req.params.id)
   );
+
   // check for found user
   if (!foundUser) {
     return res
       .status(400)
-      .json({ message: `Employee ID ${req.params.id} not found` });
+      .json({ message: `User ID ${req.params.id} not found` });
   }
+
   // send user back to client
   res.json(foundUser);
 };
 
-export { getAllUsers, getUser };
+// add a user
+const addUser = (req: Request, res: Response) => {
+  const newUser = {
+    id: data.users?.length ? data.users[data.users.length - 1].id + 1 : 1,
+    firstname: req.body.firstname,
+    lastname: req.body.lastname,
+  };
+
+  // check for first and last name
+  if (!newUser.firstname && !newUser.lastname) {
+    res.status(400).json({ message: 'First and last names are required.' });
+  }
+
+  // pass updated data back to setUsers
+  data.setUsers([...data.users, newUser]);
+  // send success code and data back to client
+  res.status(201).json(data.users);
+};
+
+// update a user
+const updateUser = (req: Request, res: Response) => {
+  // find user from id param
+  const foundUser = data.users.find(
+    (user: IUser) => user.id === parseInt(req.params.id)
+  );
+
+  // check for found user
+  if (!foundUser) {
+    return res
+      .status(400)
+      .json({ message: `User ID ${req.params.id} not found` });
+  }
+
+  // if we received a name set the user we found to the new parameter value
+  if (req.body.firstname) foundUser.firstname = req.body.firstname;
+  if (req.body.lastname) foundUser.lastname = req.body.lastname;
+
+  //  remove the existing user record from database
+  const filteredUsers = data.users.filter(
+    (user: IUser) => user.id !== parseInt(req.params.id)
+  );
+
+  // combine the array without the existing user and the new user object
+  const unsortedArray = [...filteredUsers, foundUser];
+
+  // call 'setUsers' function from the data object (react setState pattern) and sort
+  data.setUsers(
+    unsortedArray.sort((a, b) => (a.id > b.id ? 1 : a.id < b.id ? -1 : 0))
+  );
+
+  // send back the users after they have been updated by 'setUsers'
+  res.json(data.users);
+};
+
+// delete a user
+const deleteUser = (req: Request, res: Response) => {
+  // filter out the found user
+  const filteredUsers = data.users.filter(
+    (user: IUser) => user.id !== parseInt(req.params.id)
+  );
+
+  // pass filtered user array back to database (this.setUsers)
+  data.setUsers([...filteredUsers]);
+
+  // send the filtered user array back to
+  res.status(201).json(data.users);
+};
+
+export { getAllUsers, getUser, addUser, updateUser, deleteUser };
--- react/apps/rtk-query-backend/server/routes/users.route.ts
+++ react/apps/rtk-query-backend/server/routes/users.route.ts
@@ -4,6 +4,9 @@ import * as usersApi from '../controllers/users.controller';
 const usersRouter: Router = Router();
 
 usersRouter.get('/', usersApi.getAllUsers);
+usersRouter.post('/', usersApi.addUser);
+usersRouter.put('/:id', usersApi.updateUser);
+usersRouter.delete('/:id', usersApi.deleteUser);
 usersRouter.get('/:id', usersApi.getUser);
 
 export { usersRouter };
